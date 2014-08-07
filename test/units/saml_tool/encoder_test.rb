8266061037ca2ca3c12bf28de3904303f2d63377
Rob Nichols <git@nicholshayes.co.uk>
2014-08-07 09:26:47 +0100

    Tidies up encoder test

--- test/units/saml_tool/encoder_test.rb
+++ test/units/saml_tool/encoder_test.rb
@@ -5,14 +5,14 @@ module SamlTool
 
     def test_class_encode
       encoded_saml = Encoder.encode(saml)
-      inflate Base64.decode64(encoded_saml)
-      assert_equal saml, @inflated
+      inflated = inflate Base64.decode64(encoded_saml)
+      assert_equal saml, inflated
     end
 
     def test_encode
       encoded_saml = Encoder.new(saml).encode
-      inflate Base64.decode64(encoded_saml)
-      assert_equal saml, @inflated
+      inflated = inflate Base64.decode64(encoded_saml)
+      assert_equal saml, inflated
     end
 
     def test_base64
@@ -22,15 +22,16 @@ module SamlTool
 
     def test_zlib
       encoded_saml = Encoder.new(saml).zlib
-      inflate encoded_saml
-      assert_equal saml, @inflated
+      inflated = inflate encoded_saml
+      assert_equal saml, inflated
     end
 
     def inflate(encoded)
       zstream  = Zlib::Inflate.new(-Zlib::MAX_WBITS) # I have no idea why we're using minus Zlib::MAX_WBITS. Zlib documentation suggests just Zlib::MAX_WBITS should work, but it doesn't
-        @inflated = zstream.inflate(encoded)
+        inflated = zstream.inflate(encoded)
       zstream.finish
       zstream.close
+      return inflated
     end
 
   end
