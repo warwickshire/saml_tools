a0c6472a242a9cc17e9337330fe770e87409ef7e
Rob Nichols <git@nicholshayes.co.uk>
2016-09-21 14:02:58 +0100

    Add method to retrieve attributes from SAML response

--- lib/saml_tool/response_reader.rb
+++ lib/saml_tool/response_reader.rb
@@ -2,7 +2,7 @@ require "openssl"
 
 module SamlTool
   class ResponseReader < Reader
-    
+
     # On creation, the keys for this hash will be converted into methods that
     # will return the text gathered at the xpath in the matching value.
     def default_config
@@ -17,14 +17,29 @@ module SamlTool
         signature_algorithm:     '//ds:SignatureMethod/@Algorithm',
         signed_info:             '//ds:SignedInfo'
       }
-    end 
-    
-    def initialize(saml, config = {}, namespaces = {})    
+    end
+
+    def initialize(saml, config = {}, namespaces = {})
       super(
         saml,
         config.merge(default_config),
         namespaces.merge(default_namespaces)
       )
+    end
+
+    def attribute_names
+      @attribute_names ||= saml.xpath("//saml:Attribute/@Name").collect(&:value)
+    end
+
+    # A hash with the attribute names as keys, and the matching attribute value content as values.
+    # Note that if the same Name is assigned to more than one attribute or an attribute contains more than
+    # one value, then the value for that key will be an array.
+    def attributes
+      @attributes ||= attribute_names.inject({}) do |hash, name|
+        attribute_values = saml.xpath("//saml:Attribute[@Name='#{name}']/saml:AttributeValue/text()")
+        hash[name] = attribute_values.length > 1 ? attribute_values.collect(&:to_s) : attribute_values.to_s
+        hash
+      end
     end
 
     def valid?
@@ -46,19 +61,19 @@ module SamlTool
     def digests_match?
       digest_hash == decoded_digest_value
     end
-    
+
     def signatureless
       @signatureless ||= clone_saml_and_remove_signature
     end
-    
+
     def certificate
       @certificate ||= OpenSSL::X509::Certificate.new(raw_cert)
     end
-    
+
     def raw_cert
       @raw_cert ||= Base64.decode64(base64_cert)
     end
-    
+
     def fingerprint
       @fingerprint ||= Digest::SHA1.hexdigest(certificate.to_der)
     end
@@ -66,7 +81,7 @@ module SamlTool
     def signature
       @signature ||= Base64.decode64(signature_value)
     end
-    
+
     def canonicalization_algorithm
       case canonicalization_method
         when "http://www.w3.org/TR/2001/REC-xml-c14n-20010315" then Nokogiri::XML::XML_C14N_1_0
@@ -74,14 +89,14 @@ module SamlTool
         else                                                        Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
       end
     end
-    
+
     def hashed_element
       @hashed_element ||= signatureless.at_xpath("//*[@ID='#{reference_uri[1..-1]}']")
     end
-    
+
     def canonicalized_hashed_element
       hashed_element.canonicalize(
-        canonicalization_algorithm, 
+        canonicalization_algorithm,
         inclusive_namespaces.split(' ')
       )
     end
@@ -96,7 +111,7 @@ module SamlTool
     def signed_info_element
       signed_info.source.first
     end
-    
+
     def digest_algorithm_class
       @digest_algorithm_class ||= determine_algorithm_class(digest_algorithm)
     end
@@ -104,7 +119,7 @@ module SamlTool
     def signature_algorithm_class
       @signature_algorithm_class ||= determine_algorithm_class(signature_algorithm)
     end
-  
+
     def determine_algorithm_class(method_text)
       case method_text.slice(/sha(\d+)\s*$/, 1)
       when '256' then OpenSSL::Digest::SHA256
@@ -114,11 +129,11 @@ module SamlTool
         OpenSSL::Digest::SHA1
       end
     end
-    
+
     def digest_hash
       @digest_hash ||= digest_algorithm_class.digest(canonicalized_hashed_element)
     end
-    
+
     def decoded_digest_value
       Base64.decode64(digest_value)
     end
@@ -128,18 +143,18 @@ module SamlTool
       cloned_saml.xpath('//ds:Signature', namespaces).remove
       return SamlTool::SAML(cloned_saml.to_s)
     end
-    
+
     def default_namespaces
       {
         ds: dsig,
         ec: c14m
       }
     end
-    
+
     def c14m
       'http://www.w3.org/2001/10/xml-exc-c14n#'
     end
-        
+
     def dsig
       'http://www.w3.org/2000/09/xmldsig#'
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'minitest/autorun'
 require 'saml_tool'
 
 class Minitest::Test
-  
+
   def valid_xml
     '<foo>bar</foo>'
   end
@@ -21,25 +21,29 @@ class Minitest::Test
   def request_saml_erb
     contents_of 'files/request.saml.erb'
   end
-  
+
   def response_xml
     contents_of 'files/response.xml'
   end
-  
+
   def open_saml_request
     contents_of 'files/open_saml_response.xml'
   end
-  
+
+  def response_simple_attributes
+    contents_of 'files/response_simple_attributes.xml'
+  end
+
   def x509_certificate
     @x509_certificate ||= OpenSSL::PKCS12.new(
-      contents_of('files/usercert.p12'), 
+      contents_of('files/usercert.p12'),
       'hello'
     ).certificate
   end
-  
+
   def open_ssl_rsa_key
     @open_ssl_rsa_key ||= OpenSSL::PKey::RSA.new(
-      contents_of('files/userkey.pem'), 
+      contents_of('files/userkey.pem'),
       'hello'
     )
   end
